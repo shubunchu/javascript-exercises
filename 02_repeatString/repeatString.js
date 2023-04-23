79a461540682d7c9a3c1ddc0568a3dbfc3a7c9e7
Alex <shubunchu@gmail.com>
2023-04-23 19:57:51 -0400

    Solve 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    result = '';
+    for (let i = 0; i < num; i++) {
+        result += string;
+    }
+    if (num < 0) {
+        return 'ERROR';
+    } else {
+        return result;
+    }
 };
 
 // Do not edit below this line
