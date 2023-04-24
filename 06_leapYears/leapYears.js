e284dc7f0240540d2d7aa92fd64c0c3f8041abd7
Alex <shubunchu@gmail.com>
2023-04-24 00:46:12 -0400

    Solve 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,22 @@
-const leapYears = function() {
+const leapYears = function (year) {
+    // if ((year % 4 == 0) || (!(year % 100 == 0) && (year % 400 == 0))) {
+    //     return true;
+    // } else {
+    //     return false;
+    // }
 
+    if (year % 4 == 0) {
+        if (year % 100 == 0) {
+            if (year % 400 == 0) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
