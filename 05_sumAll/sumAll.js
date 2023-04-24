cc4beec89c3553a9b64c98faea9cf7234ad1ecf9
Alex <shubunchu@gmail.com>
2023-04-24 00:28:11 -0400

    Solve 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    let sum = 0;
+    if (start > end) {
+        end = [start, start = end][0];
+    }
+    if ((start || end) < 0) {
+        return "ERROR";
+    }
+    if ((typeof(start) != "number") || (typeof(end) != "number")) {
+        return "ERROR";
+    }
+    for (let i = start; i <= end; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
