4a9b8f3ce7feaac692d07bec04b945e8624c6cf8
Alex <shubunchu@gmail.com>
2023-04-24 01:05:08 -0400

    Solve 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let result = (5/9) * (temp - 32);
+  if (Number.isInteger(result)) {
+    return result;
+  } else {
+    return Number(result.toFixed(1));
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let result = (9/5 * temp) + 32;
+  if (Number.isInteger(result)) {
+    return result;
+  } else {
+    return Number(result.toFixed(1));
+  }
 };
 
 // Do not edit below this line
